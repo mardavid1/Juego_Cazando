8903f25dd3d7c1ff1a0e6ce2f4d24b6aaee18cda
mardavid1 <mardavid_01@hotmail.com>
2026-06-28 17:36:37 -0400

    Referencia, creacion del canvas y invocar onload desde body

---
+++ cazando.js
@@ -0,0 +1,17 @@
+let canvas=document.getElementById('areaJuego');
+let cxt=canvas.getContext('2d');
+
+
+function iniciar(){
+    graficarGato;
+
+}
+function graficarGato(){
+    cxt.fillstyle='brown';
+    cxt.fillRect(250,250,50,10);
+}
+
+
+
+
+
